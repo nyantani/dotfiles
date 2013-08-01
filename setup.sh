7f80309b61d9d27852c0d36b54c8348c7cd2f7aa
nyantani <taninakaakira@Poppin-MBA-123-no-MacBook-Air.local>
2013-08-01 22:18:37 +0900

    mod setup.sh

--- setup.sh
+++ setup.sh
@@ -6,10 +6,12 @@ ln -fs "$PWD/.zshrc"       ~/.zshrc
 
 ln -fs "$PWD/.vimrc"        ~/.vimrc
 ln -fs "$PWD/.tmux.conf"    ~/.tmux.conf
+ln -fs "$PWD/.gitconfig"    ~/.gitconfig
 
 git submodule init && git submodule update
 
 mkdir -p ~/.vim/bundle
+git clone git://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
 
 echo "done."
 
